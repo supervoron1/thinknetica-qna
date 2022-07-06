4391a050a95a54d2e874496642fa5164d322ea36
raven <vorontsof@mail.ru>
2022-07-06 12:03:33 +0400

    lesson5  Specs for Questions controller#new :green_circle:

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -61,4 +61,31 @@ RSpec.describe QuestionsController, type: :controller do
 
   end
 
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question in the DB' do
+        # count = Question.count
+        # post :create, params: { question: attributes_for(:question) } # using factory_bot
+        # expect(Question.count).to eq count + 1
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+
+      end
+      it 're-renders new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
 end
