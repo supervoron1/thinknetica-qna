1b4ba717f91b59ece2fce1ad0d27658e9ff4604e
raven <vorontsof@mail.ru>
2022-07-06 13:41:42 +0400

    lesson5  Specs for Questions controller#update :green_circle:

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit]
+  before_action :load_question, only: [:show, :edit, :update]
 
   def index
     @questions = Question.all
@@ -23,6 +23,15 @@ class QuestionsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+
   end
 
   private
