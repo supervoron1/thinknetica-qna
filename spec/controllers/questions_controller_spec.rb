ee7d84f108ec8b32aa1899863e58b7aa2a8b77b5
raven <vorontsof@mail.ru>
2022-07-06 10:07:05 +0400

    lesson5  Questions controller#new :green_circle:

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -35,4 +35,17 @@ RSpec.describe QuestionsController, type: :controller do
 
   end
 
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+
+  end
+
 end
